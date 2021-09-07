bf06486195ec6fb33d704492d8e9bf48de793a3b
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-09-07 13:07:48 -0500

    Added Redux
    Implemented userReducer and userActions to header and app components.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
-  <BrowserRouter basename="/React">
-    <App />
-  </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter basename="/React">
+            <App />
+        </BrowserRouter>,
+    </Provider>,
   document.getElementById('root')
 );
+
+
 
