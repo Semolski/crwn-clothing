b95e58c1110c1e2264979f9d69fc0ae06297da34
Sarah Mayoral-Osmolski <sarah@semolski.com>
2021-09-09 11:39:17 -0500

    Added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,7 +1,7 @@
-import {createSelector} from "reselect";
+
+import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
-
 
 export const selectCartItems = createSelector(
     [selectCart],
@@ -13,23 +13,22 @@ export const selectCartHidden = createSelector(
     cart => cart.hidden
 );
 
-
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
         cartItems.reduce(
-            (accumulatedQuantity, cartItem) =>
-                accumulatedQuantity + cartItem.quantity,
+            (accumalatedQuantity, cartItem) =>
+                accumalatedQuantity + cartItem.quantity,
             0
         )
-)
+);
 
 export const selectCartTotal = createSelector(
     [selectCartItems],
     cartItems =>
         cartItems.reduce(
-            (accumulatedQuantity, cartItem) =>
-                accumulatedQuantity + cartItem.quantity * cartItem.price,
+            (accumalatedQuantity, cartItem) =>
+                accumalatedQuantity + cartItem.quantity * cartItem.price,
             0
         )
-)
+);
